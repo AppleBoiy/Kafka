82cf73ea352470cad35cd29cadd7e9c9d9a33d3e
AppleBoiy <work.chaipat@gmail.com>
2024-09-17 04:51:03 +0700

    codecrafters submit [skip ci]

--- src/server.js
+++ src/server.js
@@ -23,6 +23,47 @@ function parseHeader(data) {
     };
 }
 
+function createResponse() {
+    const apiKeys = [
+        {apiKey: 18, version: 4}, // metadata
+        {apiKey: 1, version: 16}, // produce
+    ];
+
+    const ERROR_CODE_SIZE = 2;
+    const NUMBER_OF_VERSIONS_SIZE = 1;
+    const API_KEY_SIZE = 2;
+    const VERSION_SIZE = 2;
+    const THROTTLE_TIME_SIZE = 4;
+    const NUMBER_OF_TOPICS_SIZE = 1;
+
+    const bufferSize = ERROR_CODE_SIZE
+        + NUMBER_OF_VERSIONS_SIZE
+        + apiKeys.length * (API_KEY_SIZE + VERSION_SIZE * 2 + 1)
+        + THROTTLE_TIME_SIZE
+        + NUMBER_OF_TOPICS_SIZE;
+
+    const buffer = Buffer.alloc(bufferSize);
+
+    buffer.writeUInt16BE(0, 0); // error code (0 = NO_ERROR)
+    buffer.writeUInt8(apiKeys.length + 1, ERROR_CODE_SIZE); // number of versions
+
+    // Write each API key and version
+    let offset = ERROR_CODE_SIZE + NUMBER_OF_VERSIONS_SIZE;
+    apiKeys.forEach(({apiKey, version}) => {
+        buffer.writeUInt16BE(apiKey, offset);
+        buffer.writeUInt16BE(version, offset + API_KEY_SIZE);
+        buffer.writeUInt16BE(version, offset + API_KEY_SIZE + VERSION_SIZE);
+        buffer.writeUInt8(0, offset + API_KEY_SIZE + VERSION_SIZE * 2);
+        offset += API_KEY_SIZE + VERSION_SIZE * 2 + 1;
+    });
+
+    buffer.writeUInt32BE(0, offset); // throttle time
+    buffer.writeUInt8(0, offset + THROTTLE_TIME_SIZE); // number of topics
+
+    return buffer;
+}
+
+
 async function kafkaServer(socket) {
     let dataBuffer = Buffer.alloc(0);
 
@@ -46,16 +87,7 @@ async function kafkaServer(socket) {
                 ]);
                 socket.write(errorResponse);
             } else {
-                const responseData = Buffer.from([
-                    0, 0, // Error code
-                    2, // API keys length + 1
-                    0, // Reserved
-                    18, // API key
-                    0, 4, // Min version
-                    0, 4, // Max version
-                    0, 0, 0, 0, 0, // Throttle time
-                    0, // Reserved (not documented)
-                ]);
+                const responseData = createResponse();
 
                 const response = Buffer.concat([
                     Buffer.from([0, 0, 0, responseData.length + 4]), // Length
